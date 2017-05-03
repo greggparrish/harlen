23df8378e45627d359e7c2248c8e1940a6125076
Gregg Parrish <gregg.alb@gmail.com>
2017-05-03 09:29:04 +0200

    mv structural images

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@ set :branch, "master"
 set :rvm_ruby_version, '2.4.1@harlen'
 
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
-set :server_name, "harlen.greggparrish.com"
+set :server_name, "harlendev.greggparrish.com"
 
-server 'harlen.greggparrish.com', user: 'agricola', roles: %w{web app db}, primary: true
+server 'harlendev.greggparrish.com', user: 'agricola', roles: %w{web app db}, primary: true
 
 set :rails_env, :production
 set :enable_ssl, false
